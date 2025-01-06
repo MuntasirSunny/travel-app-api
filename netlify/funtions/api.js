b5fbc274b76aa62e0b86fe7a0ade7217ceea7b42
MuntasirSunny <hossainmdmuntasir@gmail.com>
2025-01-07 03:28:13 +0600

    update- again, netlify

--- netlify/funtions/api.js
+++ netlify/funtions/api.js
@@ -1,7 +1,7 @@
 const serverless = require("serverless-http");
 const express = require("express");
 
-const app = require("../../app.js");
+const app = require("../../app");
 
 // Export for Netlify
 module.exports.handler = serverless(app);
